77873aa3749fc694e710ee6884b245a35a5a8bc2
OTJOHN06 <johnitater2@gmail.com>
2020-10-28 15:06:29 +0000

    Updates

---
+++ precache-manifest.7c8ab0c7796d81c723e7cf7ed48abfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3a8b1b501f46c4031e9db6643830ca",
+    "url": "/monsters-redux/index.html"
+  },
+  {
+    "revision": "13e3bef9494dbf9a0176",
+    "url": "/monsters-redux/static/css/main.33ae977a.chunk.css"
+  },
+  {
+    "revision": "2be29f0f408acd28ef0d",
+    "url": "/monsters-redux/static/js/2.6ca9021c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/monsters-redux/static/js/2.6ca9021c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e3bef9494dbf9a0176",
+    "url": "/monsters-redux/static/js/main.40c878fb.chunk.js"
+  },
+  {
+    "revision": "cb1958b1e5f07b10e804",
+    "url": "/monsters-redux/static/js/runtime-main.292b57dd.js"
+  }
+]);
